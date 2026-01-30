321f6395b46c19b6cadc0bd3f7882615b8382bee
Droid7770 <salkovkonstantin711@gmail.com>
2026-01-30 19:01:05 +0300

    Finished 22nd program

---
+++ Sources/22.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <time.h>
+#include <locale.h>
+int main() {
+	char input;
+	clock_t timestamp1;
+	setlocale(LC_ALL, "ru_RU.UTF-8");
+	printf("Нажмите enter два раза ");
+	scanf_s("%c", &input);
+	timestamp1 = clock();
+	scanf_s("%c", &input);
+	printf("Количество времени, которое прошло после первого ввода: %.3f\n", ((double)(clock() - timestamp1)) / CLOCKS_PER_SEC);
+}
