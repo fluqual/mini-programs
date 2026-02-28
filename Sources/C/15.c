da265a36a6895521afe258070e5a7ff795ab0bf1
fluqual <salkovkonstantin711@gmail.com>
2026-02-28 21:11:53 +0300

    Finished 15th program

---
+++ Sources/C/15.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <locale.h>
+
+void swap(int* x, int* y) {
+	int temp = *x;
+	*x = *y;
+	*y = temp;
+}
+
+int partition(int *arr, int low, int high) {
+	int pivot = arr[high];
+	int j = (low - 1);
+	for (int i = low; i <= high - 1; ++i) {
+		if (arr[i] <= pivot) {
+			j++;
+			swap(&arr[j], &arr[i]);
+		}
+	}
+	swap(&arr[j + 1], &arr[high]);
+	return (j + 1);
+}
+
+void quicksort(int* arr, int low, int high) {
+	if (low < high) {
+		int pi = partition(arr, low, high);
+		quicksort(arr, low, pi - 1);
+		quicksort(arr, pi + 1, high);
+	}
+}
+
+int main() {
+	setlocale(LC_ALL, "ru_RU.UTF-8");
+	int size;
+	printf("Введите размер массива: ");
+	scanf("%i", &size);
+	printf("Введите массив:\n");
+	int *arr = (int*)malloc(sizeof(int) * size);
+	for (int i = 0; i < size; ++i) {
+		scanf("%i", &arr[i]);
+	}
+	quicksort(arr, 0, size - 1);
+    printf("Отсортированный массив: ");
+	for (int i = 0; i < size; ++i) {
+		printf("%i ", arr[i]);
+	}
+	printf("\n");
+	return 0;
+}
