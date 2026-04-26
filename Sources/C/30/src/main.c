d391676fe61ced397aa786080569486453cbf948
fluqual <salkovkonstantin711@gmail.com>
2026-04-26 19:24:37 +0300

    Changes to 30th program
    Added static pixel rendering logic and a test triangle rendering. Fixed
    CmakeLists.txt

--- Sources/C/30/src/main.c
+++ Sources/C/30/src/main.c
@@ -1,34 +1,85 @@
+#include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#include <stdio.h>
+
+void InitVertexBuffer(unsigned int* buffer) {
+    float verticies[6] = {
+        -0.5f, -0.5f,
+        0.5f, -0.5f,
+        0.0f, 0.5f
+    };
+    glGenBuffers(1, buffer);
+    glBindBuffer(GL_ARRAY_BUFFER, *buffer);
+    glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), verticies, GL_STATIC_DRAW);
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+}
+void InitPixelBuffer(unsigned int* buffer, float* position) {
+    glGenBuffers(1, buffer);
+    glBindBuffer(GL_ARRAY_BUFFER, *buffer);
+    glBufferData(GL_ARRAY_BUFFER, 2 * sizeof(float), position, GL_DYNAMIC_DRAW);
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+}
+unsigned int CreatePixel(float* position) {
+    unsigned int buffer;
+    InitPixelBuffer(&buffer, position);
+    glBindBuffer(GL_ARRAY_BUFFER, buffer);
+    return buffer;
+}
+void DrawBindedPixel() {
+        glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), (void*)0);
+        glDrawArrays(GL_POINTS, 0, 1);
+}
+
+void DrawBindedTriangle() {
+        glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), (void*)0);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+}
 
 int main(void)
 {
     GLFWwindow* window;
 
-    /* Initialize the library */
     if (!glfwInit())
         return -1;
 
-    /* Create a windowed mode window and its OpenGL context */
     window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+
     if (!window)
     {
         glfwTerminate();
         return -1;
     }
+    
 
-    /* Make the window's context current */
     glfwMakeContextCurrent(window);
 
-    /* Loop until the user closes the window */
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        printf("Failed to initialize GLAD\n");
+        return -1;
+    }
+    GLFWcursor* cursor = glfwCreateStandardCursor(GLFW_ARROW_CURSOR);
+    glfwSetCursor(window, cursor);
+
+    printf("%s\n", glGetString(GL_VERSION));
+    unsigned int buffer;
+    InitVertexBuffer(&buffer);
+    glBindBuffer(GL_ARRAY_BUFFER, buffer);
+    glEnableVertexAttribArray(0);
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    float position[2] = {0.7f, 0.0f};
+    unsigned int pixelbuffer = CreatePixel(position);
+
     while (!glfwWindowShouldClose(window))
     {
-        /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
 
-        /* Swap front and back buffers */
+        glBindBuffer(GL_ARRAY_BUFFER, pixelbuffer);
+        DrawBindedPixel();
+        glBindBuffer(GL_ARRAY_BUFFER, buffer);
+        DrawBindedTriangle();
+
         glfwSwapBuffers(window);
 
-        /* Poll for and process events */
         glfwPollEvents();
     }
 
