2d65b26c07fef6de9d3c50ce8efc79d36237f290
fluqual <salkovkonstantin711@gmail.com>
2026-04-27 11:34:18 +0300

    Added shader compilation in 30th program

--- Sources/C/30/src/main.c
+++ Sources/C/30/src/main.c
@@ -1,6 +1,42 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include <stdio.h>
+#include <stdlib.h>
+
+unsigned int CompileShader(unsigned int type, const char* source) {
+    unsigned int id = glCreateShader(type);
+    const char* src = &source[0];
+    glShaderSource(id, 1, &src, nullptr);
+    glCompileShader(id);
+    int compile_result;
+    glGetShaderiv(id, GL_COMPILE_STATUS, &compile_result);
+    if (compile_result == GL_FALSE) {
+        int length;
+        glGetShaderiv(id, GL_INFO_LOG_LENGTH, &length);
+        char* buffer = malloc(sizeof(char) * length + 1);
+        glGetShaderInfoLog(id, length, &length, buffer);
+        printf("Shader compilation failed:\n");
+        printf("%s\n", buffer);
+        free(buffer);
+        glDeleteShader(id);
+        return 0;
+    }
+    return id;
+}
+
+static unsigned int CreateShader(const char* vertexShader, const char* fragmentShader) {
+    unsigned int program = glCreateProgram();
+    unsigned int vs = CompileShader(GL_VERTEX_SHADER, vertexShader);
+    unsigned int fs = CompileShader(GL_FRAGMENT_SHADER, fragmentShader);
+    glAttachShader(program, vs);
+    glAttachShader(program, fs);
+    glLinkProgram(program);
+    glValidateProgram(program);
+
+    glDeleteShader(vs);
+    glDeleteShader(fs);
+    return program;
+}
 
 void InitVertexBuffer(unsigned int* buffer) {
     float verticies[6] = {
@@ -19,12 +55,14 @@ void InitPixelBuffer(unsigned int* buffer, float* position) {
     glBufferData(GL_ARRAY_BUFFER, 2 * sizeof(float), position, GL_DYNAMIC_DRAW);
     glBindBuffer(GL_ARRAY_BUFFER, 0);
 }
+
 unsigned int CreatePixel(float* position) {
     unsigned int buffer;
     InitPixelBuffer(&buffer, position);
     glBindBuffer(GL_ARRAY_BUFFER, buffer);
     return buffer;
 }
+
 void DrawBindedPixel() {
         glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), (void*)0);
         glDrawArrays(GL_POINTS, 0, 1);
@@ -65,6 +103,23 @@ int main(void)
     InitVertexBuffer(&buffer);
     glBindBuffer(GL_ARRAY_BUFFER, buffer);
     glEnableVertexAttribArray(0);
+
+    char vertexShader[] = 
+        "#version 330 core\n" 
+        "layout(location = 0) in vec4 position;\n"
+        "void main() {\n"
+        "   gl_Position = position;\n"
+        "}\n";
+
+    char fragmentShader[] = 
+        "#version 330 core\n" 
+        "layout(location = 0) out vec4 color;\n"
+        "void main() {\n"
+        "   color = vec4(1.0, 0.5, 1.0, 1.0);\n"
+        "}\n";
+
+    unsigned int shader = CreateShader(vertexShader, fragmentShader);
+    glUseProgram(shader);
     glBindBuffer(GL_ARRAY_BUFFER, 0);
     float position[2] = {0.7f, 0.0f};
     unsigned int pixelbuffer = CreatePixel(position);
@@ -82,6 +137,7 @@ int main(void)
 
         glfwPollEvents();
     }
+    glDeleteShader(shader);
 
     glfwTerminate();
     return 0;
